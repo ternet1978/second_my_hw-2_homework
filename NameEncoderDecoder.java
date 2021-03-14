df2876eecd75043c040f89ca78f483e6864d4da0
ternet1978 <48205597+ternet1978@users.noreply.github.com>
2021-03-14 13:00:33 +0200

    Added my class encoder

---
+++ NameEncoderDecoder.java
@@ -0,0 +1,22 @@
+public class NameEncoderDecoder {
+   
+    public String encode(String name) {
+        return "NOTFORYOU" +name
+          .replace('e', '1')
+          .replace('u', '2')
+          .replace('i', '3')
+          .replace('o', '4')
+          .replace('a', '5')
+        + "YESNOTFORYOU";
+    }
+  
+    public String decode(String name) {
+        return name.substring(9,name.length()-12)
+               .replace('1', 'e')
+               .replace('2', 'u')
+               .replace('3', 'i')
+               .replace('4', 'o')
+               .replace('5', 'a');
+    }
+  
+}
